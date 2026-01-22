a4838d8d242b1eac4f721d2a0310355b2bcf2a8d
pedro.cfandrade@gmail.com <pedro.cfandrade@gmail.com>
2026-01-22 16:17:58 -0300

    feat: FindMaxGreatness

---
+++ src/main/MaxGreatness.java
@@ -0,0 +1,21 @@
+package main;
+import java.util.*;
+
+import static java.lang.Integer.MAX_VALUE;
+
+public class MaxGreatness {
+    public static void main(String[] args){
+
+    }
+
+
+
+    public static int findMaxGreatness(List<Integer> array){
+        int n = array.size();
+
+        Collections.sort(array);
+        List<Integer> sortedArray = new ArrayList<>(array);
+
+        return 0;
+    }
+}
